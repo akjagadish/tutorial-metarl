9057c404ea17926f255885a042a9ad0d2c5a49b2
akjagadish <akshaykjagadish@gmail.com>
2020-12-07 09:38:26 +0100

    document task

--- tutorial_metarl/task/BernoulliBandit.py
+++ tutorial_metarl/task/BernoulliBandit.py
@@ -4,16 +4,28 @@ import  math
 from tutorial_metarl.trainers.utils import set_random_seed
 
 class BernoulliBandit():
-
     def __init__(self, seed, probs):
-        
+        """Bernoulli Bandit Task w/ Dependent Arms
+
+        Args:
+            seed ([int]): seed for the dataloader
+            probs ([list]): list of probs of rewards for arm 1
+        """
         self.seed = seed
         self.probs  = probs
         self.num_arms = 2
         set_random_seed(seed)
         
     def sample(self, num_episodes, prob=None):
-       
+        """sample reward per arm
+
+        Args:
+            num_episodes ([int]): number of episodes to sample rewards for
+            prob ([float], optional): probability of reward for arm 1. Defaults to None.
+
+        Returns:
+            Y [list]: rewards per arm for num_episodes
+        """
         X, Y = [], []
         for rnd in np.arange(num_episodes):
             if prob is None:
